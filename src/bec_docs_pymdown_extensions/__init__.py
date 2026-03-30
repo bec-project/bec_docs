71f1b1f18d8eaac70b4d40cc49a1f617d2d0abe2
wyzula-jan <jan.wyzula@psi.ch>
2026-03-30 15:21:16 +0200

    feat: added icons support to the tabs

--- src/bec_docs_pymdown_extensions/__init__.py
+++ src/bec_docs_pymdown_extensions/__init__.py
@@ -1,3 +1,16 @@
-from .snippet_preprocessor import make_extension as makeExtension
+from markdown.extensions import Extension
+
+from .snippet_preprocessor import TestSnippets
+from .tab_label_icons import TabLabelIcons
 
 __all__ = ["makeExtension"]
+
+
+class BecDocsExtension(Extension):
+    def extendMarkdown(self, md):
+        TabLabelIcons().extendMarkdown(md)
+        TestSnippets().extendMarkdown(md)
+
+
+def makeExtension(**kwargs):
+    return BecDocsExtension(**kwargs)
