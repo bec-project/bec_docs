ab09bd2265fb58560c9da17e343c5ad75ee3da88
David Perl <david.perl@psi.ch>
2026-03-31 09:51:38 +0200

    tests: retry checking output in snippet test

--- tests/snippet_tests/conftest.py
+++ tests/snippet_tests/conftest.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pytest
 
 from bec_docs_pymdown_extensions.matchers import ExpectedOutputMatcher
@@ -22,6 +24,12 @@ def expected_output_check(capsys, request: pytest.FixtureRequest):
             raise TestSetupError("Mark your test with an expected output matcher!")
         yield
         captured = capsys.readouterr()
-        assert matcher.check(
-            captured.out
-        ), f"Expected output matcher {type(matcher)} failed for test: {request.node.name}. Diff:\n{matcher.diff(captured.out)}"
+        if matcher.check(captured.out):
+            pass
+        else:
+            # Retry after waiting to finish
+            sleep(1)
+            captured = capsys.readouterr()
+            assert matcher.check(
+                captured.out
+            ), f"Expected output matcher {type(matcher)} failed for test: {request.node.name}. Diff:\n{matcher.diff(captured.out)}"
