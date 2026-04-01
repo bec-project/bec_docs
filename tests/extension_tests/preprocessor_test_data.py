45bda3e555b790cf982a42bb5ac54de58617f657
wyzula-jan <jan.wyzula@psi.ch>
2026-04-01 13:28:31 +0200

    test: extended preprocessor tests

--- tests/extension_tests/preprocessor_test_data.py
+++ tests/extension_tests/preprocessor_test_data.py
@@ -1,8 +1,10 @@
 import pytest
 
 from bec_docs_pymdown_extensions.matchers import ContainsExpectedOutputMatcher
+from bec_docs_pymdown_extensions.snippet_preprocessor import PLACEHOLDER_TOKEN
 
 CONSTANT_STRING = "constant string"
+PLACEHOLDER_STRING = f"prefix {PLACEHOLDER_TOKEN} suffix"
 
 INCLUDED_VALUE = "interpolated"
 F_STRING = f"{INCLUDED_VALUE} f-string"
@@ -21,3 +23,15 @@ def transform_with_constant_string():
 def snippet_with_no_output():
     code = 3
     run_function()
+
+
+@pytest.mark.expected_output(ContainsExpectedOutputMatcher(CONSTANT_STRING))
+def snippet_with_docs_display_and_hidden_lines():
+    print(render_me())  # docs-display
+    helper = render_me()  # docs-hide
+    assert helper == "ok"  # docs-hide
+
+
+@pytest.mark.expected_output(ContainsExpectedOutputMatcher(PLACEHOLDER_STRING))
+def snippet_with_placeholder_output():
+    run_placeholder_demo()
